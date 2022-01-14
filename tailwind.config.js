7e265f1a468e6db1a6a1c519d11ad7101bee37a0
Noah Rahm <correctsyntax@yahoo.com>
2022-01-14 12:04:55 -0600

    Update to new site design

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {  
+  content: ["./*.{html,js}"],  
+  theme: {       
+    fontFamily: {
+      sans: ['Work Sans', 'sans-serif'],
+    },  
+    extend: {
+      colors: {            
+        primary: '#031932',  
+        accent: '#4EA132',    
+        dark: '#030E1B',
+        lblue: '#1B2E43'   
+      } 
+    },  
+  },  
+  plugins: [],
+}
