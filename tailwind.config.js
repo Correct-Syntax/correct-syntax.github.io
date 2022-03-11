0e64ffbfd0e08131dcfbc36eb856d381b1fcd958
Noah Rahm <correctsyntax@yahoo.com>
2022-03-11 16:38:21 -0600

    Update website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@ module.exports = {
   content: ["./*.{html,js}"],  
   theme: {       
     fontFamily: {
-      sans: ['Work Sans', 'sans-serif'],
+      sans: ['Mukta', 'sans-serif'],
     },  
     extend: {
       colors: {            
-        primary: '#031932',  
-        accent: '#2D96A1',    
-        dark: '#030E1B',
-        lblue: '#1B2E43'   
+        accent: '#0B7C8D',    
       } 
     },  
   },  
