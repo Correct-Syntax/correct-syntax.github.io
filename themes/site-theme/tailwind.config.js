db62ac986450aeb51f8b812043f823f78cab6f57
Noah Rahm <correctsyntax@yahoo.com>
2023-06-19 12:45:07 -0500

    Update styles and fonts

--- themes/site-theme/tailwind.config.js
+++ themes/site-theme/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     theme: {    
         fontFamily: {
           sans: ['Figtree', 'sans-serif'],
+          serif: ['Lora', 'serif'],
         },    
         extend: {          
           colors: {          
